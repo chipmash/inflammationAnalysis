1ab5ca1a4c70b49511c7cabfee00b7d78dff3c43
Chipo.Mashayamombe <chipo.mashayamombe@postgrad.manchester.ac.uk>
2015-11-10 08:26:14 -0800

    thought I corrected the code

--- range_overlap.py
+++ range_overlap.py
@@ -1,20 +1,19 @@
 def range_overlap(ranges):
-	'''Return common ovrlap among a set of [low,high] ranges.'''
-	lowest = 0.0
-	highest = 1.0
-	for (low, high) in ranges:
-		lowest = max(lowest, low)
-		highest = min(highest, high)
-	if highest < lowest:
-		return None
-	else:		
-		return (lowest, highest)
-
-
+    '''Return common overlap among a set of [low, high] ranges.'''
+    lowest = 0.0
+    highest = 100.0
+    for (low, high) in ranges:
+      lowest = max(lowest, low)
+      highest = min(highest, high)
+    if highest < lowest:
+      return None
+    else:
+      return (lowest, highest)
 def test_range_overlap():
-        assert range_overlap([ (0.0,1.0), (5.0,6.0)]) == None
-        assert range_overlap(0.0, 1.0) == (0.0, 1.0)
-
-
+    assert range_overlap([(0.0, 1.0), (5.0,6.0)]) == None, 'Test should return none'
+    print 'test one'
+    assert range_overlap([(0.0, 1.0)]) == (0.0, 1.0), 'Test should return (0.0,  1.0)'
+    print 'test two'
+    assert range_overlap([(2.0, 3.0), (2.0, 4.0)]) == None, 'Test should return a value'
+    print 'test three'
 test_range_overlap()
-
---
+++ weight_function.py
@@ -0,0 +1,6 @@
+def weight_lb(weight_kg):
+	return 2.2 * weight_kg
+
+
+print('weight is: ', weight_lb(105))
+
